c8f6e70cb1f55868e78706aafdd3ffda11261271
luoyue <2087682597@qq.com>
2023-08-31 10:19:29 +0800

    (LY):能访问docs下的readme文件

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,8 @@
+import { defineUserConfig, } from 'vuepress'
+export default defineUserConfig({
+ base:'/',
+ lang: 'zh-CN',
+ title: 'ByteRythm 极简抖音项目文档',
+ description: '极简版抖音相关文档',
+ head: [['link', { rel: 'icon', href: '/images/logo.png' }]],
+})
